2fbf788cc84ab2de81331ca84b2797327c386f52
Juliano Carneiro <julianoomc@gmail.com>
2024-07-09 19:57:36 -0300

    conecta serviço com o Amazon SNS e envia notificação a partir da mensagem capturada na queue

--- src/main/java/com/jmc/notificacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/jmc/notificacao/listener/PropostaPendenteListener.java
@@ -4,16 +4,18 @@ import com.jmc.notificacao.constante.MensagemConstante;
 import com.jmc.notificacao.domain.Proposta;
 import com.jmc.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
+    @Autowired
     private NotificacaoSnsService notificacaoSnsService;
 
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
     public void propostaPendente(Proposta proposta) {
         String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
-        notificacaoSnsService.notificar(mensagem);
+        notificacaoSnsService.notificar(proposta.getUsuario().getTelefone(), mensagem);
     }
 }
